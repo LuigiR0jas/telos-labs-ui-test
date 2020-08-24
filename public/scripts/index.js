bc53255decd0280930684a43ccbc012136420fe1
Luigi Rojas <luigirojasdev@gmail.com>
2020-08-24 13:15:18 -0500

    review changes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-	$(".section__card-arrow").click(function (e) {
-		if ($(this).hasClass("section__card-arrow--up")) {
-			$(this).removeClass("section__card-arrow--up");
-			$(this).next().removeClass("section__card-p--visible");
+	$(".card-arrow").click(function (e) {
+		if ($(this).hasClass("card-arrow--up")) {
+			$(this).removeClass("card-arrow--up");
+			$(this).next().removeClass("card-p--visible");
 		} else {
-			$(this).addClass("section__card-arrow--up");
-			$(this).next().addClass("section__card-p--visible");
+			$(this).addClass("card-arrow--up");
+			$(this).next().addClass("card-p--visible");
 		}
 	});
 });
